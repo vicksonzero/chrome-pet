2426c08d233f6a2e2fd2b8008ee91b3d410307ef
vicksonzero <dicksonzero@gmail.com>
2016-02-15 02:35:54 +0800

    ghost file will now respond to events

--- src/background.js
+++ src/background.js
@@ -1,22 +1,41 @@
 
 
-var ghostScript = require("./ghost/jarvis/");
+var ghostScript = require("./ghost/jarvis/jarvis");
 var Ghost = require("./js/Ghost")(ghostScript);
 
-var ghostInstance;
 // onInstalled
 // onStartup
+
+var game = {
+	ghostInstance:null,
+	speak: function speak (msg) {
+		chrome.tts.speak(msg, {'voiceName': 'Google UK English Male', pitch:1.2});
+	},
+	showAllVoices: function showAllVoices () {
+		chrome.tts.getVoices(function(voices) {
+			for (var i = 0; i < voices.length; i++) {
+				console.log('Voice ' + i + ':');
+				console.log('  name: ' + voices[i].voiceName);
+				console.log('  lang: ' + voices[i].lang);
+				console.log('  gender: ' + voices[i].gender);
+				console.log('  extension id: ' + voices[i].extensionId);
+				console.log('  event types: ' + voices[i].eventTypes);
+			}
+		});
+	}
+}
+
 chrome.runtime.onInstalled.addListener(function(){
 	//showAllVoices();
 
 
-	ghostInstance = new Ghost();
-	console.log(ghostInstance);
+	game.ghostInstance = new Ghost();
+	console.log(game.ghostInstance);
 
-	greetRandom(ghostInstance);
+	game.ghostInstance.onWake(game);
 
 	var recognition = new webkitSpeechRecognition();
-	recognition.onresult = function(event) { 
+	recognition.onresult = function(event) {
 		console.log(event);
 	}
 	recognition.start();
@@ -24,28 +43,9 @@ chrome.runtime.onInstalled.addListener(function(){
 
 
 
-function greetRandom (ghost) {
-	speak(ghost.getGreetingPhrase());
-}
 
 
-function speak (msg) {
-	chrome.tts.speak(msg, {'voiceName': 'Google UK English Male', pitch:1.2});
-}
 
 
-function showAllVoices () {
-	chrome.tts.getVoices(function(voices) {
-		for (var i = 0; i < voices.length; i++) {
-			console.log('Voice ' + i + ':');
-			console.log('  name: ' + voices[i].voiceName);
-			console.log('  lang: ' + voices[i].lang);
-			console.log('  gender: ' + voices[i].gender);
-			console.log('  extension id: ' + voices[i].extensionId);
-			console.log('  event types: ' + voices[i].eventTypes);
-		}
-	});
-}
 
-
-//https://gist.github.com/danharper/8364399 
+//https://gist.github.com/danharper/8364399
--- src/ghost/jarvis/jarvis.js
+++ src/ghost/jarvis/jarvis.js
@@ -1,46 +1,81 @@
+//http://ihgs.blog.163.com/blog/static/194117102201103010618477/
+
 module.exports = {
 	name: "jarvis",
 	state: "default",
 	states: ["default"],
+	dat:{
+		minTimeTillBored:10, // 10 seconds
+		dateBirthday: 0,
+		dateLastWakeup: 0,
+		timeSlept: 0,
+		lastSpoken: 0
+	},
+
+	onGhostSelected: function(game){
+		this.greetRandom(game);
+	},
+	onWake: function(game){
+		this.greetRandom(game);
+	},
+	onOpenSettings: function(game){
+
+	},
+	onBored: function(game) {
+
+	},
+	onMessage:function(game) {
+
+	},
+	greetRandom: function greetRandom (game) {
+		game.speak(this.getGreetingPhrase());
+	},
 
 	getGreetingPhrase: function getGreetingPhrase(){
-		var greetings = this.scripts.greetings;
-		console.log(this);
+		var greetings = this.dictionary.greetings.list;
+		//console.log(greetings);
 		var index = Math.floor(Math.random()*greetings.length);
 		//var index = 0;
 		return greetings[index];
 	},
 
-	scripts:{
-		greetings:[
-			"Oh... it's YOU. It's been a long time, how have you been?",
-			"Are you still there?",
-			"If life gives you lemon, make combustible lemon grenades!",
-			"I'm different",
-			"Prometheus was punished by the gods for giving the gift of knowledge to man. He was cast into the bowels of the Earth and pecked by birds.",
-			// C&C
-			"Establishing battlefield control, standby...",
-			"Battle control online",
-			"Our base is under attack",
-			"I've got a present for ya",
-			"Silos needed",
-			"Tiberium exposure detected",
+	dictionary:{
 
-			// Starcraft
-			"You want a piece of me boy?",
-			"Nuclear launch detected.",
-			"My life for hire!",
-			"Spawn more overlordssss...",
-			"You must construct additional pylons.",
-			"Additional supply depots required.",
-			"Power overwhelming!",
-			"For the Hor... Wait... Go, go, go!",
-			// League of Legends
-			"Crisp to the maximum",
-			// Toy Story
-			"There's a snake in your boot!",
-		]
-	}
-	
+		greetings:{
+			nonoverlap:10, // sequential:true, nonoverlap:0.5
+			list:[
+				"Oh... it's YOU. It's been a long time, how have you been?",
+				"Are you still there?",
+				"If life gives you lemon, make combustible lemon grenades!",
+				"I'm different",
+				"Prometheus was punished by the gods for giving the gift of knowledge to man. He was cast into the bowels of the Earth and pecked by birds.",
+				// C&C
+				"Establishing battlefield control, standby...",
+				"Battle control online",
+				"Our base is under attack",
+				"I've got a present for ya",
+				"Silos needed",
+				"Tiberium exposure detected",
+
+				// Starcraft
+				"You want a piece of me boy?",
+				"Nuclear launch detected.",
+				"My life for hire!",
+				"Spawn more overlords...",
+				"You must construct additional pylons.",
+				"Additional supply depots required.",
+				"Power overwhelming!",
+				"For the Hor... Wait... Go, go, go!",
+				// League of Legends
+				"Crisp to the maximum",
+				// Toy Story
+				"There's a snake in your boot!",
+			]
+		}
+	},
+
+
+	// plugin data
+	plugins:{}
 
 }
